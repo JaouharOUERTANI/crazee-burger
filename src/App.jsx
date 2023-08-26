194c9deedd3c57e9ba56ad01714f8d32fc246851
jaouhar ouertani <jaouhar.ouertani@gmail.com>
2023-08-26 17:26:58 +0200

    ✨ F01: LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import React from 'react'; // N'oubliez pas d'importer React
-import reactLogo from './assets/react.svg';
-import './App.css';
+
+import LoginPage from "./components/pages/LoginPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="App-logo" alt="logo" /> {/* Utilisez reactLogo */}
-        <p>SALUT JOUJOU</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Lien vers React
-        </a>
-      </header>
-    </div>
-  );
+  return <LoginPage />
 }
 
 export default App;
